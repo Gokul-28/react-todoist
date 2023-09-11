43e25c9df1f50ed8d7d79aab22c55273f9c15704
Gokul S <87302750+Gokul-28@users.noreply.github.com>
2023-09-11 19:52:01 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Check from "./component/Check";
+
+function App() {
+  return (
+    <div>
+      <Check />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Check.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Check = () => {
+  return (
+    <div class="alert alert-primary" role="alert">
+    <strong>Well done!</strong> You successfully read this
+    important alert message.
+</div>
+
+  )
+}
+
+export default Check
